8b5aac95d8e692defdfa11cbf504681f5c4d137f
NVPL1234 <nlam39784@gmail.com>
2022-09-27 22:42:53 +0700

    xong cr data

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const multer = require('multer')
 const express = require('express')
+const data = require('./store')
+
 const app = express()
+const upload = multer()
 
 app.use(express.json({ extended: false }))
 app.use(express.static('./views'))
@@ -7,10 +11,13 @@ app.set('view engine', 'ejs')
 app.set('views', './views')
 
 app.get('/', (request, response) => {
-    return response.render('index')
+    return response.render('index', {data: data})
 })
 
-app.post('/', )
+app.post('/', upload.fields([]), (request, response)=>{
+    data.push(request.body)
+    return response.redirect('/')
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!')
